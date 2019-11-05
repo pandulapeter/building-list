5380b829c8d33cc2c886b49f5be7fb9db9d523a0
Péter Pandula <peter@halcyonmobile.com>
2019-11-05 14:45:26 +0200

    Add files for the empty Android project.

---
+++ app/src/main/java/com/pandulapeter/buildinglist/BuildingListActivity.kt
@@ -0,0 +1,12 @@
+package com.pandulapeter.buildinglist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BuildingListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+    }
+}
